6d44e8644550729e38a008c6f22c83907a2c8e7e
kamino <lishichenjs@163.com>
2019-04-20 18:50:36 +0800

    修复不明错误

--- BiliLive/src/robot.py
+++ BiliLive/src/robot.py
@@ -8,7 +8,6 @@ from .config import Config
 from .study import StudyExt
 from .file import File
 from .timer import Timer
-from .danmu import DanmuHandle
 
 
 class Robot(object):
@@ -16,7 +15,7 @@ class Robot(object):
     @staticmethod
     def text_msg(user_id, user_name, content):
         """处理文本消息"""
-        # content = content.replace('~喵', '')  # 愚人节
+        # content = content.greplace('~喵', '')  # 愚人节
         File.add(Config.config('root-path') + 'BiliLive/save/danmu.log',
                  f'[{Timer.stamp2str(Timer.timestamp())}] {user_id}({user_name}): {content}\n')
         if user_name == Config.config('auth')['name']:
@@ -27,7 +26,6 @@ class Robot(object):
                 return rank
             return '打卡失败，不知道为啥'
         if content == 'sudo reboot':
-            DanmuHandle.send("RESTART AT %s" % Timer.stamp2str(Timer.timestamp(), '%H:%M:%S'))
             print("SUDO RESTART")
             os.system("ps -ef | grep run.py | grep -v grep | awk '{print $2}' | xargs --no-run-if-empty kill")
 
